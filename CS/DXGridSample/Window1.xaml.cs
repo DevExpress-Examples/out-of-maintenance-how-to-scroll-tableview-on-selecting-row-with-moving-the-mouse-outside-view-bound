8c2e5afd185da17b4df2f51d8f0863234928e607
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXGridSample/Window1.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DXGrid_AssignComboBoxToColumn {
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            grid.ItemsSource = NwindData.Data;
+        }
+
+        private int StartRowHandle = -1;
+        private int CurrentRowHandle = -1;
+
+
+
+        private void TableView_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            StartRowHandle = GetRowAt(e.OriginalSource as DependencyObject);
+        }
+
+        private void TableView_MouseMove(object sender, MouseEventArgs e)
+        {
+            int newRowHandle = GetRowAt(e.OriginalSource as DependencyObject);
+            if (CurrentRowHandle != newRowHandle)
+            {
+                CurrentRowHandle = newRowHandle;
+                SelectRows(StartRowHandle, CurrentRowHandle);
+            }
+        }
+
+        private void TableView_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            StartRowHandle = -1;
+            CurrentRowHandle = -1;
+        }
+        private int GetRowAt(DependencyObject sourceObj)
+        {
+            return myTableView.CalcHitInfo(sourceObj).RowHandle;
+        }
+        private void SelectRows(int startRow, int endRow)
+        {
+            if (startRow > -1 && endRow > -1)
+            {
+                myTableView.BeginSelection();
+                myTableView.ClearSelection();
+                myTableView.SelectRange(startRow, endRow);
+                myTableView.EndSelection();
+            }
+        }
+
+    }
+    public class NwindData {
+        public static nwindDataSet.ProductsDataTable Data {
+            get { return new nwindDataSetTableAdapters.ProductsTableAdapter().GetData(); }
+        }
+    }
+}
---
+++ VB/DXGridSample/Window1.xaml.vb
@@ -0,0 +1,68 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+
+Namespace DXGrid_AssignComboBoxToColumn
+	Partial Public Class Window1
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			grid.ItemsSource = NwindData.Data
+		End Sub
+
+		Private StartRowHandle As Integer = -1
+		Private CurrentRowHandle As Integer = -1
+
+
+
+		Private Sub TableView_MouseDown(ByVal sender As Object, ByVal e As MouseButtonEventArgs)
+			StartRowHandle = GetRowAt(TryCast(e.OriginalSource, DependencyObject))
+		End Sub
+
+		Private Sub TableView_MouseMove(ByVal sender As Object, ByVal e As MouseEventArgs)
+			Dim newRowHandle As Integer = GetRowAt(TryCast(e.OriginalSource, DependencyObject))
+			If CurrentRowHandle <> newRowHandle Then
+				CurrentRowHandle = newRowHandle
+				SelectRows(StartRowHandle, CurrentRowHandle)
+			End If
+		End Sub
+
+		Private Sub TableView_MouseUp(ByVal sender As Object, ByVal e As MouseButtonEventArgs)
+			StartRowHandle = -1
+			CurrentRowHandle = -1
+		End Sub
+		Private Function GetRowAt(ByVal sourceObj As DependencyObject) As Integer
+			Return myTableView.CalcHitInfo(sourceObj).RowHandle
+		End Function
+		Private Sub SelectRows(ByVal startRow As Integer, ByVal endRow As Integer)
+			If startRow > -1 AndAlso endRow > -1 Then
+				myTableView.BeginSelection()
+				myTableView.ClearSelection()
+				myTableView.SelectRange(startRow, endRow)
+				myTableView.EndSelection()
+			End If
+		End Sub
+
+	End Class
+	Public Class NwindData
+		Public Shared ReadOnly Property Data() As nwindDataSet.ProductsDataTable
+			Get
+				Return New nwindDataSetTableAdapters.ProductsTableAdapter().GetData()
+			End Get
+		End Property
+	End Class
+End Namespace
